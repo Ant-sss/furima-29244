b187d1433a7145e44e32065c1b38fc53d47906f0
Ant-sss <mimo.antownette.0429@gmail.com>
2020-09-04 18:06:39 +0900

    ActiveStorageを追加とnewページ表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one :buy
+  has_one_attached :image
+
+  validates :name, :text, :category, :delivery_fee, :ship_from, :days, :price, presence: true
 end
