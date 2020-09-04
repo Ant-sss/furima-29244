761ff0b46ecbd93442c8fa0019cc3cc967445ba8
Ant-sss <mimo.antownette.0429@gmail.com>
2020-09-04 16:45:31 +0900

    正規表現を変数に代入

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  PASSWORD_REGEX = /([0-9].*[a-zA-Z]|[a-zA-Z].*[0-9])/
+  NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+  KANA_REGEX = /\A[ァ-ヶー－]+\z/
   
   validates :nickname,:birthday, presence: true
-  validates :password, presence: true, format: { with: /([0-9].*[a-zA-Z]|[a-zA-Z].*[0-9])/ }
-  validates :first_name, :last_name, presence: true, format: { with:/\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name_kana, :last_name_kana, presence: true, format: { with:/\A[ァ-ヶー－]+\z/ }
+  validates :password, presence: true, format: { with: PASSWORD_REGEX }
+  validates :first_name, :last_name, presence: true, format: { with: NAME_REGEX }
+  validates :first_name_kana, :last_name_kana, presence: true, format: { with: KANA_REGEX }
   has_many :items
   has_many :buys
 end
