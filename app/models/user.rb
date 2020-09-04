e1567d846601dec016115a55a5921dddb3c8c80d
Ant-sss <mimo.antownette.0429@gmail.com>
2020-09-04 12:17:18 +0900

    deviseの導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday, presence: true
+
+  has_many :items
+  has_many :buys
 end
