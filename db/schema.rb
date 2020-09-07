cd360d20569f5f4894a751991bae6331b5b52c12
Ant-sss <mimo.antownette.0429@gmail.com>
2020-09-07 15:44:13 +0900

    出品画面の設定

--- db/schema.rb
+++ db/schema.rb
@@ -36,11 +36,11 @@ ActiveRecord::Schema.define(version: 2020_09_04_084853) do
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.text "text", null: false
-    t.integer "category", null: false
-    t.integer "status", null: false
-    t.integer "delivery_fee", null: false
-    t.integer "ship_from", null: false
-    t.integer "days", null: false
+    t.integer "category_id", null: false
+    t.integer "status_id", null: false
+    t.integer "deliveryfee_id", null: false
+    t.integer "shipfrom_id", null: false
+    t.integer "day_id", null: false
     t.integer "price", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
