78301acfdbbb697c08d76d3bca8ed36edbb272d6
Ant-sss <mimo.antownette.0429@gmail.com>
2020-09-11 10:23:42 +0900

    payjpの導入

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,16 +1,20 @@
 class BuysController < ApplicationController
+  before_action :set_item, only: [:index, :create]
   
   def index
     @buy = Buy.new
-    @item = Item.find(params[:item_id])
+    # @item = Item.find(params[:item_id]) # 車のおもちゃを取得
+    @address = Address.new
   end
 
-  def create
+  def create # @itemが空になっているので再度@itemを定義してあげる
+    # @item = Item.find(params[:item_id]) # 車のおもちゃを取得
     @address = Address.new(address_params)
     if @address.save
+      pay_item
       redirect_to root_path
     else
-      render :index
+      render "index"
     end
   end
 
@@ -18,6 +22,23 @@ class BuysController < ApplicationController
 
   def address_params
     params.permit(:postal_code, :prefecture_id, :city, :building_name, :phone_num)
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def buy_params
+    params.permit(:token)
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_ff136e8748e55e57632f8aae"
+    Payjp::Charge.create(
+      amount: set_item[:price],
+      card: buy_params[:token],
+      currency:'jpy'
+    )
   end
 
 end
