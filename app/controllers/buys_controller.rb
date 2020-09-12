4ba7ffbda577300fd56c6f990da52b2b882c4ea1
Ant-sss <mimo.antownette.0429@gmail.com>
2020-09-12 17:41:37 +0900

    画面遷移の制限

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,5 +1,6 @@
 class BuysController < ApplicationController
   before_action :set_item, only: [:index, :create]
+  before_action :move_to_signed_in
   before_action :move_to_index
   
   def index
@@ -42,9 +43,16 @@ class BuysController < ApplicationController
     )
   end
 
-  def move_to_index
+  def move_to_signed_in
     unless user_signed_in?
       redirect_to new_user_session_path
+    end
+  end
+
+  def move_to_index
+    @item = Item.find(params[:item_id])
+    if current_user.id == @item.user_id
+      redirect_to root_path
     end
   end
 
